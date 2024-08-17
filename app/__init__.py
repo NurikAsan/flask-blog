188286ad99def3846093fdff676fae512409d3f1
Nurikov <asankylovnursultan@gmail.com>
2024-08-17 14:11:03 +0600

    base page

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+mail = Mail()
+login_manager.login_view = 'auth.login'
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    login_manager.init_app(app)
+    mail.init_app(app)
+    db.init_app(app)
+    return app
